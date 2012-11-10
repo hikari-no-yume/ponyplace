bc2234aa8931565501b21fffcf9be980efc5d373
Andrew Faulds <ajf@ajf.me>
2012-11-10 13:25:01 +0000

    No consecutive colour repetition for party background

--- htdocs/static/rooms/party.js
+++ htdocs/static/rooms/party.js
@@ -11,14 +11,19 @@
         vidwindow.innerHTML = '<iframe width="320" height="240" src="http://www.youtube.com/embed/videoseries?list=PLuyBiw2l8OdpAGly7cY6Ar5YgYovW2Ps7&amp;hl=en_US&amp;autoplay=1&amp;loop=1" frameborder="0" allowfullscreen></iframe>';
         document.body.appendChild(vidwindow);
 
+        var lastColor = '';
         setInterval(function () {
-            document.body.style.backgroundColor = 'rgba('
-                + Math.floor(Math.random()*2)*255
-                + ','
-                + Math.floor(Math.random()*2)*255
-                + ','
-                + Math.floor(Math.random()*2)*255
-                + ',0.5)';
+            do {
+                var newColor = 'rgba('
+                    + Math.floor(Math.random()*2)*255
+                    + ','
+                    + Math.floor(Math.random()*2)*255
+                    + ','
+                    + Math.floor(Math.random()*2)*255
+                    + ',0.5)'
+            } while (lastColor === newColor)
+            document.body.style.backgroundColor = newColor;
+            lastColor = newColor;
         }, 400);
         document.onclick = function (e) {
            ponyplace.doMove(e.layerX, e.layerY);
